383e318f1b98784664d52afad56ec67184a10e05
Olga <vol.ol.vl@yandex.ru>
2025-01-21 03:13:17 +0300

    пример состояния и изменения state классового компонента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,35 @@ import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
+import { Component } from 'react';
+class WhoAmI extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            years: 27,
+            text: '+++'
+        }
+    }
+
+    nextYear = () => {
+        this.setState(state => ({
+            years: state.years + 1
+        }))
+    }
+
+    render() {
+        const {name, surname, link} = this.props;
+        return (
+            <div>
+                <button onClick={this.nextYear}>{this.state.text}</button>
+                <h1>My name {name}, surname - {surname}, age - {this.state.years}</h1>
+                <a href={link}>My profile</a>
+            </div>
+        )
+    }
+
+}
+
 function App() {
     const data = [
         {name: 'Jhon C.', salary: 800, increase: true, id: 1},
@@ -14,6 +43,9 @@ function App() {
 
     return (
         <div className="app">
+            <WhoAmI name='Jhon' surname='Smith' link='vk.com' />
+            <WhoAmI name='Alex' surname='V' link='vk.com' />
+
             <AppInfo />
 
             <div className="search-panel">
