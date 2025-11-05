da221cd8123e67c0df3d1044529dbf27b40cd63b
Olga <vol.ol.vl@yandex.ru>
2025-11-05 19:48:01 +0300

    Панель поиска

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
                 {name: 'Jhon C.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Carl W.', salary: 15000, rise: false, id: 3},
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -83,9 +84,26 @@ class App extends Component {
         }))
     }
 
+    serchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+            // item.name.contains(term)
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.serchEmp(data, term);
 
         return (
             <div className="app">
@@ -94,11 +112,11 @@ class App extends Component {
                     increased = {increased} />
     
                 <div className="search-panel">
-                    <SearchPanel />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter />
                 </div>
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp} />
                 <EmployeesAddForm
