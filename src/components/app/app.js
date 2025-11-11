405aafa18e0919a66949c88aede35e09bd7db715
Olga <vol.ol.vl@yandex.ru>
2025-11-12 02:11:36 +0300

    фильтры

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,8 @@ class App extends Component {
                 {name: 'Alex M.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Carl W.', salary: 15000, rise: false, id: 3},
             ],
-            term: ''
+            term: '',
+            filter: 'all'
         }
         this.maxId = 4;
     }
@@ -86,7 +87,7 @@ class App extends Component {
 
     serchEmp = (items, term) => {
         if (term.length === 0) {
-            return items;
+            return items
         }
 
         return items.filter(item => {
@@ -99,11 +100,26 @@ class App extends Component {
         this.setState({term})
     }
 
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.rise);
+            case 'moreThen1000':
+                return items.filter(item => item.salary > 1000);
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
+
     render() {
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length;
-        const visibleData = this.serchEmp(data, term);
+        const visibleData = this.filterPost(this.serchEmp(data, term), filter);
 
         return (
             <div className="app">
@@ -113,7 +129,9 @@ class App extends Component {
     
                 <div className="search-panel">
                     <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-                    <AppFilter />
+                    <AppFilter 
+                        filter={filter}
+                        onFilterSelect={this.onFilterSelect} />
                 </div>
                 <EmployeesList 
                     data={visibleData}
